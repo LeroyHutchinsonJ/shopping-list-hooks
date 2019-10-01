cd9b864edfc81f2cb8527187affacdb8ba318dbd
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-09-30 23:55:28 -0400

    Added add item action
    Added an add item action to the shopping list

--- src/shoppingList.js
+++ src/shoppingList.js
@@ -1,17 +1,47 @@
 import React, { useRef, useReducer } from "react";
 
 const reducer = (state, action) => {
-  switch (
-    action.type
-    //Do something with the action
-  ) {
+  switch (action.type) {
+    case "add":
+      return [...state, { id: state.length, name: action.name }];
+    case "remove":
+      return [];
+
+    default:
+      return state;
   }
 };
 
 export default function ShoppingList() {
   const inputRef = useRef();
-
   const [items, dispatch] = useReducer(reducer, []);
 
-  return <div>Non sense</div>;
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    dispatch({
+      type: "add",
+      name: inputRef.current.value
+    });
+    inputRef.current.value = "";
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input ref={inputRef} />
+      </form>
+      <ul>
+        {items.map((item, index) => (
+          <li key={item.id}>
+            {item.name}{" "}
+            <button onClick={() => dispatch({ type: "remove", index })}>
+              {" "}
+              X{" "}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
