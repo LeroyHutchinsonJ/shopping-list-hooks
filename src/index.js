ad54b25fd5262bc07263d51a6a8d0e093ed65b2e
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-09-30 22:08:29 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+import React, { useRef, useReducer } from "react";
+import "./styles.css";
+
+function App() {
+  return <div className="App" />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
